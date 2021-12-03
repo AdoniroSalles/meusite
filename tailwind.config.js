02bb786ac51f4320eecc11df0476197cc071fd3d
Adoniro Salles <adonirosalles@gmail.com>
2021-12-03 15:07:54 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    '.src/components/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: (theme) => ({
+        '10v': '10vh',
+        '20v': '20vh',
+        '30v': '30vh',
+        '40v': '40vh',
+        '50v': '50vh',
+        '60v': '60vh',
+        '70v': '70vh',
+        '80v': '80vh',
+        '90v': '90vh',
+        '100v': '100vh',
+      }),
+      colors: {
+        'blue-default'  :'#40739C',
+        'yeallow-default' : '#FBBF24'
+      }
+    },
   },
   variants: {
     extend: {},
